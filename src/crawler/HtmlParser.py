c27b4a0e61a0e344800c907f59ab8544ed2fe58d
nolanator19 <nolanator19@localhost>
2012-04-23 16:32:51 -0700

    html parser file created

---
+++ src/crawler/HtmlParser.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 23, 2012
+
+@author: Nolan
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
