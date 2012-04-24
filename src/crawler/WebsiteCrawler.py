d42102fc309f76e6a81ba3664156dc96928aa074
nolanator19 <nolanator19@localhost>
2012-04-23 19:43:08 -0700

    now it can filter to mostly just articles

--- src/crawler/WebsiteCrawler.py
+++ src/crawler/WebsiteCrawler.py
@@ -21,24 +21,33 @@ class WebsiteCrawler(object):
         Constructor
         '''
         self.mech = mechanize.Browser()
-    def get_links(self,url):
-        self.mech.open(url)
+    def get_links(self,baseurl):
+        self.mech.open(baseurl)
         response = self.mech.response()
         #print response.info()
         #print response.read()
-        links = self.mech.links(url_regex=url)
+        links = self.mech.links(url_regex=baseurl)
         articles = []
-        for link in self.mech.links(url_regex=url):
-#            print link.url
-            articles.append(link.url)
-        for url in articles:
-            print url
-
+        for link in self.mech.links(url_regex=baseurl):
+#            print link.url        
+            nurl = self.__normalize_url(link.url)
+            if len(nurl) - nurl.rfind("/") > 20 and len(nurl) > len(baseurl):
+                articles.append(nurl)
+        
+        for name in set(articles):
+            print name
             
+    def __normalize_url(self, url):
+            if url.find("?") != -1:
+                return url[0:url.find("?")]
+            else:
+                return url
+
         
         
      
 
 
 crawler = WebsiteCrawler()
-print crawler.get_links("http://seattletimes.nwsource.com/")
+print crawler.get_links("http://www.nytimes.com")
+
