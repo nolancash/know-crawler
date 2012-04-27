9f09affe0c3cd636c2b7d87bad324fa781a55b77
nolanator19 <nolanator19@localhost>
2012-04-27 02:40:07 -0700

    can now actually parse a front page and get all article data

--- src/crawler/ArticleParser.py
+++ src/crawler/ArticleParser.py
@@ -15,21 +15,20 @@ class ArticleParser(HTMLParser):
     '''
     classdocs
     '''
-    done = 0
-    ignoreline = -1
-    gottext = 0
-    html = ""
-    firstonly = [0,0,0,0,0,0,0]
-
     def __init__(self):
         '''
         Constructor
         '''
         HTMLParser.__init__(self)
+        self.__done = 0
+        self.__ignoreline = -1
+        self.__gottext = 0
+        self.__html = ""
+        self.__firstonly = [0,0,0,0,0,0,0]
         self.mech = mechanize.Browser()
         
     '''
-    removes script tags from html so htmlparser doesn't break
+    removes script tags from __html so htmlparser doesn't break
     '''
     @staticmethod
     def pre_parse(html, tag):
@@ -43,7 +42,7 @@ class ArticleParser(HTMLParser):
                 end = html.find("</" + tag + ">")
                 firsthalf = html[:start]
                 secondhalf = html[end + len("<\\" + tag + ">"):]
-#                print html[start:end]
+#                print __html[start:end]
                 html = firsthalf + secondhalf
                 if end ==  -1:
                     start = -1
@@ -52,22 +51,22 @@ class ArticleParser(HTMLParser):
     def get_HTML(self, url):
         self.mech.open(url)
         response = self.mech.response()
-        self.html = response.read()
-        return self.html
+        self.__html = response.read()
+        return self.__html
 
     def get_tag_by_name(self, tag, attrs, isfound):
         found_description = 0
-        if (self.firstonly[isfound] == 0):
+        if (self.__firstonly[isfound] == 0):
             for attr in attrs:
                 if attr[1].find(tag) != -1:
                     found_description = 1
                     print attr[1]
                 if attr[0].find("content") != -1 and found_description == 1:
                     print attr[1]
-                    self.firstonly[isfound] = 1
+                    self.__firstonly[isfound] = 1
     
     def handle_starttag(self, tag, attrs):
-        if self.ignoreline < self.getpos()[0]:
+        if self.__ignoreline < self.getpos()[0]:
             if tag == "meta":
 #                print "Encountered a start tag:", tag
                 self.get_tag_by_name("title", attrs, 0)
@@ -78,43 +77,43 @@ class ArticleParser(HTMLParser):
                 self.get_tag_by_name("time", attrs,5)  
                 self.get_tag_by_name("type", attrs,5)
             if tag == "p":
-                self.gottext = 1
+                self.__gottext = 1
    
     def handle_data(self,data):
-#        if self.gottext:
+#        if self.__gottext:
 #            print data
-        self.gottext = 0
+        self.__gottext = 0
 
     def handle_endtag(self, tag):
-        if tag == "html":
-            self.done = 1
-            print "done"
+        if tag == "__html":
+            self.__done = 1
+            print "__done"
                 
     def delete_line(self, data, pos):
         print "deleted line" + str(pos[0])
         res = data.split("\n")
         print "res lines" + str(len(res))
         res.pop(pos[0]-1)
-        self.ignoreline = pos[0]
+        self.__ignoreline = pos[0]
         return "\n".join(res)
     
     def skip_broken_line(self):
-        temphtml = self.html
+        temphtml = self.__html
         pos = self.getpos()
         self.reset()
-        self.html = self.delete_line(temphtml, pos)
+        self.__html = self.delete_line(temphtml, pos)
         
 #parser = ArticleParser()
 
-#parser.get_HTML("http://www.nytimes.com/2012/04/26/us/considering-arizona-immigration-law-justices-are-again-in-political-storm.html")
-#html = parser.get_HTML("http://www.nytimes.com/2012/04/26/us/considering-arizona-immigration-law-justices-are-again-in-political-storm.html")
-##html = parser.get_HTML("http://www.aljazeera.com/news/asia-pacific/2012/04/201242733733409278.html")
-##while (parser.done == 0):
+#parser.get_HTML("http://www.nytimes.com/2012/04/26/us/considering-arizona-immigration-law-justices-are-again-in-political-storm.__html")
+#__html = parser.get_HTML("http://www.nytimes.com/2012/04/26/us/considering-arizona-immigration-law-justices-are-again-in-political-storm.__html")
+##__html = parser.get_HTML("http://www.aljazeera.com/news/asia-pacific/2012/04/201242733733409278.__html")
+##while (parser.__done == 0):
 ##    try:
 #print "running"
-#html = ArticleParser.pre_parse(html, "script")
+#__html = ArticleParser.pre_parse(__html, "script")
 #print "finished"
-#parser.feed(html)
+#parser.feed(__html)
 #    except:
 #        print "Unexpected error:", sys.exc_info()
 #        parser.skip_broken_line()
