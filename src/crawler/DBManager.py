0e74a6c4be75e088ada612d80871f06b42f611ff
tyoshiro <tyoshiro@uw.edu>
2012-05-02 21:15:55 -0700

    Added tests for the database manager.

--- src/crawler/DBManager.py
+++ src/crawler/DBManager.py
@@ -36,22 +36,25 @@ class DBManager(object):
         print query
         if title != "null" and description != "null" and url != "null":
             self.conn.query(query)
+            return True
+        return False
         
     def add_article_list(self, article):
-        count=0
-        print article[6]
-        query = "select * from articles a where a.url like \"%" + article[6] + "%\";"
-        #self.conn.query(query)
-        #r = self.conn.store_result()
-        curs = self.conn.cursor()
-        curs.execute(query)
-        rows = curs.fetchall()
-        print len(rows)
-        #print r.fetch_row(0)
-        #for row in r.fetch_row(0):
-        #    count += 1
-        if len(rows) == 0:
-            self.add_article_info(article[0], article[1], article[2], article[3], article[4], article[6])
+#        count=0
+        if len(article) == 7:
+            query = "select * from articles a where a.url like \"%" + article[6] + "%\";"
+            #self.conn.query(query)
+            #r = self.conn.store_result()
+            curs = self.conn.cursor()
+            curs.execute(query)
+            rows = curs.fetchall()
+            print len(rows)
+            #print r.fetch_row(0)
+            #for row in r.fetch_row(0):
+            #    count += 1
+            if len(rows) == 0:
+                return self.add_article_info(article[0], article[1], article[2], article[3], article[4], article[6])
+        return False
     
     def print_database(self):
         self.conn.query("select * from articles;")
