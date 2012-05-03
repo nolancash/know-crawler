ea77128eeef842f322cd7e75c9e0b824fbadbaaa
tyoshiro <tyoshiro@uw.edu>
2012-05-03 11:52:05 -0700

    Renamed to main and added dry run option.

--- src/crawler/__main__.py
+++ src/crawler/__main__.py
@@ -15,25 +15,31 @@ This script runs the web crawler. In future releases it will also manage the mul
 Parses out the passed arguments for the url to be crawled.
 """
 def parse_arguments():
-    parser = optparse.OptionParser(description="Process url.")
+    parser = optparse.OptionParser(description="know-crawler-0.1.1")
     parser.add_option("-u", dest="SOURCE_URL",
         help="The url of the news source to be processed.")
+    parser.add_option("-d", action="store_false", dest="DRY_RUN", default=True,
+                      help="Does a dry run by not inserting articles into the database.")
     return parser.parse_args()
 
 """
 Crawls the passed arguments url and saves all data to the sql database.
 """
-def main(args):
+def main(options):
     db = DBManager.DBManager()
     crawler = WebsiteCrawler.WebsiteCrawler()
-    if args.SOURCE_URL == None:
+    if options.SOURCE_URL == None:
         print "knowcrawler.zip: try knowcrawler.zip -u http://www.nytimes.com/"
         
-    if args.SOURCE_URL == "http://www.nytimes.com/":
-        print args.SOURCE_URL
-        results = crawler.parse_articles(crawler.get_links(args.SOURCE_URL))
-        for article in results:
-            db.add_article_list(article)
+    if options.SOURCE_URL == "http://www.nytimes.com/":
+        print options.SOURCE_URL
+        results = crawler.parse_articles(crawler.get_links(options.SOURCE_URL))
+        if options.DRY_RUN:
+            print "Regular run."
+            for article in results:
+                db.add_article_list(article)
+        else:
+            print "Dry Run."
         #db.print_database()
     db.close()
   
