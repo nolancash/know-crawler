3afe66b5de59b9a4ef9fcdc75e1de5e419c799fd
tyoshiro <tyoshiro@uw.edu>
2012-05-12 17:02:18 -0700

    Added word frequency methods under utilities.

--- src/crawler/Utilities.py
+++ src/crawler/Utilities.py
@@ -1,8 +1,10 @@
 '''
 Created on May 9, 2012
 
-@author: Nolan
+@author: Tyler, Nolan
 '''
+
+import re
 
 class Utilities(object):
     _instance = None
@@ -11,6 +13,36 @@ class Utilities(object):
             cls._instance = super(Utilities, cls).__new__(
                                 cls, *args, **kwargs)
         return cls._instance
+    
+    """
+    Converts text input into a list of words.
+    """
+    def word_list(self, text, ignoreCase = True):    
+        # Changes all text to lowercase.
+        if ignoreCase:
+            text = text.lower()
+    
+        # Removes ASCII apostrophes from the text.
+        text = text.replace("'", "")
+        
+        # Replaces all non-word character with spaces to represent word boundaries.
+        rx = re.compile("[^\w ]")
+        text = rx.sub(" ", text)
+        
+        # Splits words that are divided by spaces.
+        text = text.split()
+        
+        # If any "empty words" have been manufactured by previous processes, drop them.
+        text = [x for x in text if x != ""]
+        return text
+    
+    """
+    Takes a list of words and returns list of unique words and their frequencies.
+    """
+    def word_frequencies(self, text):
+        words = self.word_list(text)
+        uniqueWords = set(words)
+        return sorted([(words.count(word), word) for word in uniqueWords])
 
 
 if __name__ == '__main__':
@@ -20,3 +52,8 @@ if __name__ == '__main__':
         print "Same"
     else:
         print "Different"
+        
+u = Utilities()
+a = "apple-dog +cat #$%^&*() 1920         42orange  banana dog\ndog's cat's apples, Tyler's dog's apple's cat.dsasdsa\nqwerty:asdfg;zxcv"
+b = u.word_frequencies(a)
+print b
