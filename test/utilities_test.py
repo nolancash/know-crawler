cef3286a2e543d321877d9bfc30b859f63dcfd8b
tyoshiro <tyoshiro@uw.edu>
2012-05-12 17:07:41 -0700

    Added utilities_test module.

---
+++ test/utilities_test.py
@@ -0,0 +1,18 @@
+'''
+Created on May 12, 2012
+
+@author: Tyler
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
