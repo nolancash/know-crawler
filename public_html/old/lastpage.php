42f24a8010a0b4125439e749532570ca97568185
cbutcher <cbutcher@uw.ed>
2012-05-22 14:19:01 -0700

    Added CSS file for front page

---
+++ public_html/old/lastpage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Thank You</title>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+           <link href="frontpage.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+<div>
+	<h1>Your Changes Have Been Saved</h1>
+</div>
+
+
+	</body>
+</html>
