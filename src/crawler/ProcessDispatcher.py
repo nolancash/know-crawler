4330d107d738b9b3c6ba42c5dddd555e282f3d90
tyoshiro <tyoshiro@uw.edu>
2012-05-24 22:42:41 -0700

    Refactored the logging information.

--- src/crawler/ProcessDispatcher.py
+++ src/crawler/ProcessDispatcher.py
@@ -13,11 +13,11 @@ import os
 from multiprocessing import Pool
 
 """
-This script runs the web crawler. In future releases it will also manage the multiprocessing of the crawler.
+This script runs the web crawler.
 """
 
 """
-Parses out the passed arguments for the url to be crawled.
+Parses out the passed arguments.
 """
 def parse_arguments():
     parser = optparse.OptionParser(description="know-crawler-0.1.1")
@@ -34,77 +34,92 @@ Crawls the passed arguments url and saves all data to the sql database.
 """
 def main(options):
     # Redirects standard output to a log file.
-    log_dir = "crawler_logs"
-    log_name = __log_file_name()
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    os.chdir("./" + log_dir + "/")
-    os.makedirs(log_name)
-    os.chdir("./" + log_name + "/")
+    __log_file_setup()
+    
+    db1 = DBManager.DBManager()
     
     if options.SOURCE_URL:
+        sys.stdout = open("log_0.txt" , "w")
         print "Running on " + str(options.SOURCE_URL) + "."
-        db = DBManager.DBManager()
+        
         crawler = WebsiteCrawler.WebsiteCrawler()
         results = crawler.parse_articles(crawler.get_links(options.SOURCE_URL))
         for article in results:
-            db.add_article_list(article, options.DRY_RUN)
+            db1.add_article_list(article, options.DRY_RUN)
     else:     
         divisor = int(options.NUM_PROCESSES)
         process_lists = []
-        db = DBManager.DBManager()
+
 #        rows = db.send_query("select nsource_url from news_sources")
-        rows = db.send_query("select * from user_list")
-        counter = 0
+        rows = db1.send_query("select * from user_list")
+        
+        # Creates lists of sources for processes.
+        # The first element is dedicated for the dry run option.
+        # The second element is dedicated for the process number.
         for i in range(0,divisor):
             process_lists.append([options.DRY_RUN, i])
+            
+        # Removes unused processes.
+        for i in range(len(rows), divisor):
+            process_lists.pop()
+            
+        # Adds news sources to process lists.
+        counter = 0
         for row in rows:
             process_lists[counter % divisor].append(row[0])
             counter += 1
-#        for i in range(0, divisor):
-#            __run_from_list(process_lists[i])
+
+        # Executes parallel processing.
         pool = Pool(processes=divisor)
-        pool.map(__run_from_list, process_lists)  
-        db.close()
+        pool.map(__run_from_list, process_lists)
+        
+    # Closes the database.
+    db1.close()
 
 def __run_from_list(websites):
     dry_run = websites.pop(0)
     log_id = str(websites.pop(0))
+             
+    sys.stdout = open("log_" + log_id + ".txt" , "w")
+
+    print "Dry run: " + str(dry_run)
+    print "Running on list of news sources."   
     
-    if len(websites) > 0:
-            
-        sys.stdout = open("log_" + log_id + ".txt" , "w")
+    db = DBManager.DBManager()
+    for site in websites:
+        print site
+        crawler = WebsiteCrawler.WebsiteCrawler()
+        links = crawler.get_links(site)
+        try:
+            results = crawler.parse_articles(links)
+        except TimeoutException.TimeoutException:
+            print "Timeout Exception (outer)."
+        if results:
+            for article in results:
+                db.add_article_list(article, dry_run)
+        del crawler
     
-        print "Dry run: " + str(dry_run)
-        print "Running on list of news sources."   
-        
-        db = DBManager.DBManager()
-        for site in websites:
-            print site
-            crawler = WebsiteCrawler.WebsiteCrawler()
-            links = crawler.get_links(site)
-            try:
-                results = crawler.parse_articles(links)
-            except TimeoutException.TimeoutException:
-                print "Timeout Exception (outer)."
-            if results:
-                for article in results:
-                    db.add_article_list(article, dry_run)
-            del crawler
-        
-        db.close()
-        print "\nDone."
+    db.close()
+    print "\nDone."
 
 """
 Concatenates the file name for the log file in the format:
 log_yyyy-mm-dd_hh.mm.ss.txt
 """
-def __log_file_name():
+def __log_file_setup():
     date = datetime.datetime.now().replace(microsecond = 0)
     date = str(date)
     date = date.replace(" ", "_")
     date = date.replace(":", ".")
-    return "log_" + date
+    log_name = "log_" + date
+    
+    # Redirects standard output to a log file.
+    log_dir = "crawler_logs"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    os.chdir("./" + log_dir + "/")
+    os.makedirs(log_name)
+    os.chdir("./" + log_name + "/")
     
   
 if __name__ == "__main__":
