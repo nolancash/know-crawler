08f9a3971683250644f914a4b8f6ca29f0ca5f8a
rsukkerd <rsukkerd@uw.edu>
2012-05-30 10:15:04 -0700

    added mechanize and run_crawler.sh

---
+++ site/scheduler/mechanize/test2.py
@@ -0,0 +1,7 @@
+import MySQLdb
+import mechanize
+import signal
+
+signal.SIGALRM
+
+print "hello"
---
+++ site/scheduler/run_crawler.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=/rc12/d04/knowcse2/mechanize
+python /rc12/d04/knowcse2/know-crawler-0.3.2.zip -p 4 &
