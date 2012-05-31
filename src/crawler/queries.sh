5963896a155a5e8c5f7c402ae5e314f50b24cd3b
tyoshiro <tyoshiro@uw.edu>
2012-05-30 19:03:10 -0700

    Added queries.sh

---
+++ src/crawler/queries.sh
@@ -0,0 +1,4 @@
+python final_demo.py -k "syria massacre assad"
+python final_demo.py -k "facebook stock"
+python final_demo.py -l "colombia"
+python final_demo.py -k "romney nomination" -s 2012-05-29 -e 2012-05-30
